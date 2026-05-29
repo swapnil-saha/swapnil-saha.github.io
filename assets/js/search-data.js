c399ece9a9c008cb497574312e77bc6cbf156d22
swapnil-saha <swapnilsaha65@gmail.com>
2026-05-29 02:22:14 +0000

    Deploying to gh-pages from @ swapnil-saha/swapnil-saha.github.io@2d3f26299b70bc56590547f945ddfd3dbd3cf0fc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,21 +37,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/books/";
-              },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/blog/";
-              },
-            },{id: "post-useful-blog-posts",
+        },{id: "post-useful-blog-posts",
         
           title: "Useful Blog Posts",
         
