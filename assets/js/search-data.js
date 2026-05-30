0193adfe6d41c3dba3632c0780af9ef8a6c5079e
swapnil-saha <swapnilsaha65@gmail.com>
2026-05-30 03:44:27 +0000

    Deploying to gh-pages from @ swapnil-saha/swapnil-saha.github.io@7cbfd29edd795de6c4013f56f3a395811b47ce10 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-writing",
-          title: "writing",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/blog/";
-          },
-        },{id: "nav-research-work",
+  },{id: "nav-research-work",
           title: "research work",
           description: "Highlighted Project",
           section: "Navigation",
